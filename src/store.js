90f7466aa7a3b561ba817adfc5f45fdb336ecd8e
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-05-15 15:04:55 +0200

    feat: connect server selection menu to vuex

--- src/store.js
+++ src/store.js
@@ -6,16 +6,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    servers: []
+    servers: [],
+    // this does not need to an array, we want to store a single selection:
+    // selectedServer: []
+    selectedServer: null
   },
 
   getters: {
-    getServersList: state => state.servers
+    getServersList: state => state.servers,
+    getChoosedServer: state => state.selectedServer
   },
 
   mutations: {
     SET_SERVERS: (state, list) => {
       state.servers = list;
+    },
+
+    SET_CHOOSE_SERVER: (state, server) => {
+      // since this is a single selection we can just assign it
+      // state.selectedServer.splice(0, server);
+      state.selectedServer = server;
     }
   },
 
@@ -23,6 +33,10 @@ export default new Vuex.Store({
     async fetchServers({ commit }) {
       const { data } = await axios.get("https://xivapi.com/servers");
       commit("SET_SERVERS", data);
+    },
+
+    changeServer(context, server) {
+      context.commit("SET_CHOOSE_SERVER", server);
     }
   }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,38 +2,64 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <div>       
-          <v-select
-            :items= "serversList"
-            label="Choose your server">
-          </v-select>
-       </div>
+        <div>
+          <v-select :items="serversList" label="Choose your server" v-model="server"></v-select>
+          <!-- Let's temporarily add the value from vuex here to debug -->
+          {{choosedServer}}
+        </div>
+        <!-- 
+          Exercise 2
+          add another form item, an input field with a button that allow the user
+          to input a name and click the button to search (we will write this action together), 
+          use vuex to store your data
+          you can use what you learned in exercise one to build this.
+        -->
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { mapGetters } from 'vuex'
-// EXERCISE 1: create a selection menu that allow the user to select a server: 
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+// EXERCISE 1: create a selection menu that allow the user to select a server:
 // https://vuetifyjs.com/en/components/selects
 // use the store and build on top of what I've made for you
 export default {
   components: {},
-  created () {
+
+  created() {
     this.fetchServers();
   },
   methods: {
     ...mapActions({
-      fetchServers: 'fetchServers'
+      fetchServers: "fetchServers",
+      changeServer: "changeServer"
     })
+    // this method we do not need
+    // NewServer(server) {
+    //   this.changeServer(server);
+    // }
   },
 
- computed:{
+  computed: {
     ...mapGetters({
-      serversList: "getServersList"
-    })
-  },
+      serversList: "getServersList",
+      choosedServer: "getChoosedServer"
+    }),
+    // We are going to use the second form of computed properties,
+    // normally computed are READONLY,
+    // this way we can specify what happens when we set a computed property value:
+    server: {
+      // Like this we can specify WHERE the value is taken
+      get() {
+        return this.choosedServer;
+      },
+      //  and what to do when the value update
+      set(value) {
+        this.changeServer(value);
+      }
+    }
+  }
 };
 </script>
